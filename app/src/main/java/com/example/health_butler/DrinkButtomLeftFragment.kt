ebcbd2771bc8c54b36b8df1fb064e303bc2631f5
lman666 <1720889474@qq.com>
2022-06-23 17:46:33 +0800

    page eleventh version

--- app/src/main/java/com/example/health_butler/DrinkButtomLeftFragment.kt
+++ app/src/main/java/com/example/health_butler/DrinkButtomLeftFragment.kt
@@ -1,6 +1,7 @@
 package com.example.health_butler
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,12 +16,14 @@ class DrinkButtomLeftFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_drink_buttom_left, container, false)
+        val view = inflater.inflate(R.layout.fragment_drink_buttom_left, container, false)
+        return view
     }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         drinking_target.setOnClickListener {
+
         }
     }
 }

fae46e6278e8b30123fbd8b498fd0519707de254
AlexYuYuan <978176125@qq.com>
2022-06-23 17:54:02 +0800

    create foodadapter

--- app/src/main/java/com/example/health_butler/FoodSelectActivity.kt
+++ app/src/main/java/com/example/health_butler/FoodSelectActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.example.health_butler.data.Food
+import java.util.ArrayList
 
 class FoodSelectActivity : Fragment(){
     override fun onCreateView(
@@ -15,28 +18,37 @@ class FoodSelectActivity : Fragment(){
     ): View? {
         val view = inflater.inflate(R.layout.activity_weightrecord, container, false)
 
-        class FoodAdapter : RecyclerView.Adapter<FoodAdapter.ViewHolder>() {
+        class FoodAdapter(val foodData: List<Food>) : RecyclerView.Adapter<FoodAdapter.ViewHolder>() {
 
             inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-                
+                val foodName = view.findViewById<TextView>(R.id.food_name)
+                val calorie = view.findViewById<TextView>(R.id.calorie)
             }
 
-            override fun onCreateViewHolder(
-                parent: ViewGroup,
-                viewType: Int
-            ): FoodAdapter.ViewHolder {
-                TODO("Not yet implemented")
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodAdapter.ViewHolder {
+                return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.food_item, parent, false))
             }
 
             override fun onBindViewHolder(holder: FoodAdapter.ViewHolder, position: Int) {
-                TODO("Not yet implemented")
+                holder.foodName.setText(foodData.get(position).name)
+                holder.calorie.setText(foodData.get(position).calorie)
             }
 
             override fun getItemCount(): Int {
-                TODO("Not yet implemented")
+                return foodData.size
             }
+
         }
+
 
         return view
     }
+    interface DialogListener {
+        fun refreshActivity(unit: String)
+    }
+
+    interface ItemClickListener {
+        fun onItemClickListener(position: Int)
+    }
+
 }

78a66433f1385b8c26afdef13af475f956420823
lman666 <1720889474@qq.com>
2022-06-23 16:00:07 +0800

    page tenth version

--- app/src/main/java/com/example/health_butler/DrinkButtomLeftFragment.kt
+++ app/src/main/java/com/example/health_butler/DrinkButtomLeftFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.buttom_setdrinking_dialog.*
+import kotlinx.android.synthetic.main.fragment_drink_buttom_left.*
 
 class DrinkButtomLeftFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +16,11 @@ class DrinkButtomLeftFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_drink_buttom_left, container, false)
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        drinking_target.setOnClickListener {
+        }
     }
 }
--- app/src/main/java/com/example/health_butler/DrinkButtomRightFragment.kt
+++ app/src/main/java/com/example/health_butler/DrinkButtomRightFragment.kt
@@ -1,10 +1,19 @@
 package com.example.health_butler
 
+import android.app.TimePickerDialog
+import android.app.TimePickerDialog.OnTimeSetListener
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_drink_buttom_right.*
+import java.util.*
+
 
 class DrinkButtomRightFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +22,47 @@ class DrinkButtomRightFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_drink_buttom_right, container, false)
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        
+        switch1.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked) {
+                Log.v("aaa", "bbb")
+            }
+            else {
+                Log.v("aaa", "ccc")
+            }
+        }
+
+        add.setOnClickListener {
+            val _timePickerDialog : TimePickerDialog
+            val hourOfDay = 2
+            val minute = 2
+            val is24HourView = true
+            var hourS = ""
+            var minuteS = ""
+
+            _timePickerDialog = TimePickerDialog(activity, android.R.style.Theme_Holo_Light_Dialog,
+                OnTimeSetListener { timePicker, i, i1 -> //*Return values
+                    if(i < 10) {
+                        hourS = "0" + "$i"
+                    }
+                    else {
+                        hourS = "$i"
+                    }
+                    if(i1 < 10) {
+                        minuteS = "0" + "$i1"
+                    }
+                    else {
+                        minuteS = "$i1"
+                    }
+                    setTime.setText("$hourS:$minuteS")
+                }, hourOfDay, minute, is24HourView
+            )
+            _timePickerDialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            _timePickerDialog.setTitle("Select a Time")
+            _timePickerDialog.show()
+        }
     }
 }

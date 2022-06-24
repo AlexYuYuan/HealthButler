c596f0af1d14a48caa7adfa700313dace2154aa3
AlexYuYuan <96682294+AlexYuYuan@users.noreply.github.com>
2022-06-24 18:04:33 +0800

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit ca59c220de4195bfddd14252cabfea87b3d08468, reversing
    changes made to 317a4c182466c97ce0402b10e53f0e24b964a6df.

--- app/src/main/java/com/example/health_butler/DrinkButtomLeftFragment.kt
+++ app/src/main/java/com/example/health_butler/DrinkButtomLeftFragment.kt
@@ -5,20 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.buttom_setdrinking_dialog.*
 import kotlinx.android.synthetic.main.fragment_drink_buttom_left.*
 
-
 class DrinkButtomLeftFragment : Fragment() {
-
-    var targetDrinking = 0   //目标饮水量
-    var currentDrinking = 0   //目前饮水量
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,35 +23,7 @@ class DrinkButtomLeftFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         drinking_target.setOnClickListener {
-            showDialog(0)
+
         }
-
-        add.setOnClickListener {
-            showDialog(1)
-        }
-    }
-
-    fun showDialog(num : Int) {
-        val btnsheet = layoutInflater.inflate(R.layout.buttom_setdrinking_dialog, null)
-        val dialog = BottomSheetDialog(this.requireContext())
-        dialog.setContentView(btnsheet)
-
-        val idBtnDismiss : Button? = dialog.findViewById<Button>(R.id.idBtnDismiss)
-        val setDrinking : EditText? = dialog.findViewById<EditText>(R.id.setDrinking)
-
-        idBtnDismiss?.setOnClickListener {
-            if (num == 0) {
-                targetDrinking = setDrinking?.text.toString().toInt()
-                total_drinking.text = targetDrinking.toString() + " ml"
-            }
-            else if (num == 1) {
-                currentDrinking =  setDrinking?.text.toString().toInt()
-                current_drinking.text = currentDrinking.toString()
-            }
-//            targetDrinking = setDrinking?.text.toString().toInt()
-//            total_drinking.text = targetDrinking.toString() + " ml"
-            dialog.dismiss()
-        }
-        dialog.show()
     }
 }
